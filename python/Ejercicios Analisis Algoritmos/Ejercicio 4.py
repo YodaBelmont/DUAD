873e0fa4d6506648eaa2c3be44e7a67e369597c5
YodaBelmont <yeshuarv@hotmail.com>
2026-05-08 02:07:03 -0600

    Ejercicio 4 fixed

--- python/Ejercicios Analisis Algoritmos/Ejercicio 4.py
+++ python/Ejercicios Analisis Algoritmos/Ejercicio 4.py
@@ -1,4 +1,6 @@
-# o(Log N)
+# Es o(1) ya que el bucle for se ejecuta 10 si tiene 10 elementos,
+# si tiene menos elementos se ejecuta esa misma cantidad; ej si tiene 7 se ejecuta 7 veces ya que min() compara el 10 y el len y se queda con el menor
+# Pero si tiene mas de 10 se ejecuta solo 10 veces
 
 
 def print_10_or_less_elements(list_to_print):
