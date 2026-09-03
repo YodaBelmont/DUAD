7c42f5efce82b1cc12cac391b6215dbec889b9ad
YodaBelmont <yeshuarv@hotmail.com>
2026-09-03 01:22:18 -0600

    Ejercicios SQL Folder added

---
+++ Ejercicios SQL/query2.sql
@@ -0,0 +1,10 @@
+-- SELECT *
+--     FROM products;
+
+-- SELECT price
+--     FROM products
+--     WHERE price < 300;
+
+-- SELECT id_product
+--     FROM products_per_invoice
+--     WHERE id_product = 2
---
+++ Ejercicios SQL/query4.sql
@@ -0,0 +1,6 @@
+-- ALTER TABLE invoices
+-- ADD COLUMN buyer_phone_number TEXT NOT NULL DEFAULT '';
+
+
+-- ALTER TABLE invoices
+-- ADD COLUMN employee_id INT;
